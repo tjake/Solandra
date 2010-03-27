662fe11cc7cf68829ed8cfe791d53fd3d838d1ec
T Jake Luciani <jake@3.rdrail.net>
2010-03-27 00:02:47 -0400

    fixed demo shell script

--- run_demo.sh
+++ run_demo.sh
@@ -4,7 +4,6 @@ CASSANDRA_HOST=127.0.0.1
 CASSANDRA_PORT=9160
 CASSANDRA_FRAMED=false
 
-java -cp lucandra.jar:lucandra-tests.jar:lib/libthrift-r917130.jar:lib/log4j-1.2.15.jar:\
-lib/lucene-core-2.4.1.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-log4j12-1.5.8.jar:config \
+java -cp $( echo *.jar lib/*.jar config | sed 's/ /:/g') \
 -Dcassandra.host=${CASSANDRA_HOST} -Dcassandra.port=${CASSANDRA_PORT} -Dcassandra.framed=${CASSANDRA_FRAMED} \
 lucandra.demo.BookmarksDemo $*
