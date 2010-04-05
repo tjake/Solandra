4d6396459b290195048e8c1ed856a7e0be04c201
T Jake Luciani <jake@3.rdrail.net>
2010-04-04 23:41:44 -0400

    added shutdown hook for importer, removed unused data from solr example

--- test/lucandra/wikipedia/WikipediaImporter.java
+++ test/lucandra/wikipedia/WikipediaImporter.java
@@ -34,7 +34,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class WikipediaImporter {
-    
+
     private ExecutorService threadPool;
     private Queue<Future<Integer>> resultSet;
     private int pageCount;
@@ -42,58 +42,56 @@ public class WikipediaImporter {
     private long size;
     private long startTime;
     private long lastTime;
-    
-    
+
     public WikipediaImporter() {
         threadPool = Executors.newFixedThreadPool(64);
-        resultSet  = new LinkedBlockingQueue<Future<Integer>>();
+        resultSet = new LinkedBlockingQueue<Future<Integer>>();
         pageCount = 0;
-	loadCount = 0;
-	size      = 0;
+        loadCount = 0;
+        size = 0;
 
         startTime = System.currentTimeMillis();
-        lastTime  = System.currentTimeMillis();
+        lastTime = System.currentTimeMillis();
     }
-    
-    private static void usage(){
+
+    private static void usage() {
         System.err.println("WikipediaImporter file.xml");
         System.exit(0);
     }
-    
 
-    private void readFile(String fileName) throws IOException{
-        
-        
-        InputStream    inputFile  = new FileInputStream(fileName);   
+    private void readFile(String fileName) throws IOException {
+
+        InputStream inputFile = new FileInputStream(fileName);
         BufferedReader fileStream = new BufferedReader(new InputStreamReader(inputFile));
-        
-        //rather than xml parse, just do something fast & simple.
+
+        // rather than xml parse, just do something fast & simple.
         String line;
         Article page = new Article();
         boolean inText = false;
 
-        while( (line = fileStream.readLine()) != null){
-            
-            //Page
-            if(line.contains("<doc>")){
-                page = new Article();           
+        while ((line = fileStream.readLine()) != null) {
+
+            // Page
+            if (line.contains("<doc>")) {
+                page = new Article();
                 continue;
             }
-            
-            if(line.contains("</doc>")){
-               
-		if(++pageCount % 1000 == 0){
-		    Future<Integer> result;
 
-                    while( (result = resultSet.poll()) != null){
+            if (line.contains("</doc>")) {
+
+                if (++pageCount % 5000 == 0) {
+                    Future<Integer> result;
+
+                    while ((result = resultSet.poll()) != null) {
                         try {
                             size += result.get();
-			    loadCount++;
-			    long now = System.currentTimeMillis();
-			    if((now-lastTime)/1000.0 > 1){
-				System.err.println("Loaded ("+loadCount+") "+size/1000.0+"Kb, in "+(now-startTime)/1000.0+", avg "+(loadCount/((now-startTime)/1000))+" docs/sec");
-				lastTime = now;
-			    }			    
+                            loadCount++;
+                            long now = System.currentTimeMillis();
+                            if ((now - lastTime) / 1000.0 > 1) {
+                                System.err.println("Loaded (" + loadCount + ") " + size / 1000.0 + "Kb, in " + (now - startTime) / 1000.0 + ", avg "
+                                        + (loadCount / ((now - startTime) / 1000)) + " docs/sec");
+                                lastTime = now;
+                            }
                         } catch (InterruptedException e) {
                             // TODO Auto-generated catch block
                             e.printStackTrace();
@@ -102,119 +100,110 @@ public class WikipediaImporter {
                             e.printStackTrace();
                         }
                     }
-         
-               }
-               
-               indexPage(page);  //index each page           
+
+                }
+
+                indexPage(page); // index each page
             }
-            
-            //title
-            if(line.contains("<title>")){
-                page.title = line.substring(line.indexOf("<title>")+7, line.indexOf("</title>"));
-               
+
+            // title
+            if (line.contains("<title>")) {
+                page.title = line.substring(line.indexOf("<title>") + 7, line.indexOf("</title>"));
+
                 continue;
             }
-            
-            //url
-            if(line.contains("<url>")){
-                if(page.url == null)
-                    page.url     = line.substring(line.indexOf("<url>")+5,line.indexOf("</url>"));
-                
+
+            // url
+            if (line.contains("<url>")) {
+                if (page.url == null)
+                    page.url = line.substring(line.indexOf("<url>") + 5, line.indexOf("</url>"));
+
                 continue;
             }
-            
-            
-            //article text
-            if(line.contains("<abstract>")){
-                
-                if(line.contains("</abstract>")){
-                    page.text = line.substring(line.indexOf("<abstract>")+10, line.indexOf("</abstract>")).getBytes();
-                }else{              
-                    page.text = line.substring(line.indexOf("<abstract>"+10)).getBytes();
+
+            // article text
+            if (line.contains("<abstract>")) {
+
+                if (line.contains("</abstract>")) {
+                    page.text = line.substring(line.indexOf("<abstract>") + 10, line.indexOf("</abstract>")).getBytes();
+                } else {
+                    page.text = line.substring(line.indexOf("<abstract>" + 10)).getBytes();
                     inText = true;
                     continue;
                 }
             }
-            
-                      
-            if(inText){
-                
+
+            if (inText) {
+
                 String text = line;
-                if(line.contains("</abstract>"))
-                    text = line.substring(0,line.indexOf("</abstract>"));
-                
-                
-                byte[] newText = new byte[page.text.length+text.getBytes().length];
-                    
+                if (line.contains("</abstract>"))
+                    text = line.substring(0, line.indexOf("</abstract>"));
+
+                byte[] newText = new byte[page.text.length + text.getBytes().length];
+
                 System.arraycopy(page.text, 0, newText, 0, page.text.length);
                 System.arraycopy(text.getBytes(), 0, newText, page.text.length, text.getBytes().length);
-                    
+
                 page.text = newText;
-            }       
-            
-            
-            if(line.contains("</abstract>")){
+            }
+
+            if (line.contains("</abstract>")) {
                 inText = false;
                 continue;
             }
         }
 
-
-	threadPool.shutdown();
-	try{
-	    threadPool.awaitTermination(90, TimeUnit.SECONDS);
-	}catch(InterruptedException ex){
-	    
-	}
-
-	Future<Integer> result;
-	int size = 0;
-	while( (result = resultSet.poll()) != null){
-	    try {
-		size += result.get();
-		
-	    } catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (ExecutionException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	}
-
-	long now = System.currentTimeMillis();	
-	System.err.println("Loaded ("+pageCount+") "+size/1000+"Kb, in "+(now-lastTime)/1000.0);
-	
-	
-	System.err.println("done");
-
-    }
-    
-    public void indexPage(Article page){
-        
-       Future<Integer> result = threadPool.submit(new WikipediaIndexWorker(page));     
-       resultSet.add(result); 
-       
-    }
-    
-    public static void main(String[] args){
-        
-        
+        threadPool.shutdown();
         try {
-        
-            if(args.length > 0)
+            threadPool.awaitTermination(90, TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+
+        }
+
+        Future<Integer> result;
+        int size = 0;
+        while ((result = resultSet.poll()) != null) {
+            try {
+                size += result.get();
+
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        long now = System.currentTimeMillis();
+        System.err.println("Loaded (" + pageCount + ") " + size / 1000 + "Kb, in " + (now - lastTime) / 1000.0);
+
+        System.err.println("done");
+
+    }
+
+    public void indexPage(Article page) {
+
+        Future<Integer> result = threadPool.submit(new WikipediaIndexWorker(page));
+        resultSet.add(result);
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            if (args.length > 0)
                 new WikipediaImporter().readFile(args[0]);
             else
                 WikipediaImporter.usage();
-           
-            
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        
+
     }
-    
+
 }
--- test/lucandra/wikipedia/WikipediaIndexWorker.java
+++ test/lucandra/wikipedia/WikipediaIndexWorker.java
@@ -22,95 +22,104 @@ package lucandra.wikipedia;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import lucandra.CassandraUtils;
+import lucandra.IndexWriter;
 
 import org.apache.cassandra.thrift.Cassandra;
 import org.apache.cassandra.thrift.TokenRange;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.SimpleAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Index;
 import org.apache.lucene.document.Field.Store;
 import org.apache.thrift.transport.TTransportException;
 
+public class WikipediaIndexWorker implements Callable<Integer> {
 
-public class WikipediaIndexWorker implements Callable<Integer>{
-
-    //each worker thread has a connection to cassandra
+    // each worker thread has a connection to cassandra
+    private static ConcurrentLinkedQueue<lucandra.IndexWriter> allClients = new ConcurrentLinkedQueue<IndexWriter>();
     private static ThreadLocal<lucandra.IndexWriter> clientPool = new ThreadLocal<lucandra.IndexWriter>();
     private static ThreadLocal<Integer> batchCount = new ThreadLocal<Integer>();
-    
+
+    // get ring info
     private static List<TokenRange> ring;
-    static{
-        try{
-            Cassandra.Iface client = CassandraUtils.createConnection();            
+    static {
+        try {
+            Cassandra.Iface client = CassandraUtils.createConnection();
             ring = client.describe_ring(CassandraUtils.keySpace);
-        }catch( Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        
-        //System.err.print(ring);
+    }
+
+    //Add shutdown hook for batched commits to complete
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                lucandra.IndexWriter w;
+                while ((w = allClients.poll()) != null) {
+                    w.commit();
+                }
+
+                System.err.println("committed");
+            }
+        });
     }
     
-    
-    //this is shared by all workers
-    private static Analyzer   analyzer = new SimpleAnalyzer();
-    
-    //this is the article to index
-    private Article   article;  
-    
-    public WikipediaIndexWorker(Article article){      
+    // this is shared by all workers
+    private static Analyzer analyzer = new StandardAnalyzer();
+
+    // this is the article to index
+    private Article article;
+
+    public WikipediaIndexWorker(Article article) {
         this.article = article;
     }
-    
-    private lucandra.IndexWriter getIndexWriter() throws TTransportException{
+
+    private lucandra.IndexWriter getIndexWriter() throws TTransportException {
         lucandra.IndexWriter indexWriter = clientPool.get();
-        
-       
-        if(indexWriter == null){
-            
+
+        if (indexWriter == null) {
+
             Random r = new Random();
             List<String> endpoints = ring.get(r.nextInt(ring.size())).endpoints;
-	    String endpoint = endpoints.get(r.nextInt(endpoints.size()));
+            String endpoint = endpoints.get(r.nextInt(endpoints.size()));
 
-            indexWriter = new lucandra.IndexWriter("wikipedia",CassandraUtils.createConnection(endpoint,9160,false));
+            indexWriter = new lucandra.IndexWriter("wikipedia", CassandraUtils.createConnection(endpoint, 9160, false));
             clientPool.set(indexWriter);
 
-	    indexWriter.setAutoCommit(false);
+            indexWriter.setAutoCommit(false);
 
-
-	    batchCount.set(0);
-
+            batchCount.set(0);
         }
-        
-        return indexWriter;      
+
+        return indexWriter;
     }
-    
+
     public Integer call() throws Exception {
-        
+
         lucandra.IndexWriter indexWriter = getIndexWriter();
-        
+
         Document d = new Document();
-        
-        d.add(new Field("title",article.title,Store.YES, Index.ANALYZED));
-        
-        if(article.text != null)
-            d.add(new Field("text", new String(article.text),Store.YES, Index.ANALYZED));
-        
-        d.add(new Field("url",article.url,Store.YES, Index.NOT_ANALYZED));
-        
+
+        d.add(new Field("title", article.title, Store.YES, Index.ANALYZED));
+
+        if (article.text != null)
+            d.add(new Field("text", new String(article.text), Store.YES, Index.ANALYZED));
+
+        d.add(new Field("url", article.url, Store.YES, Index.NOT_ANALYZED));
+
         indexWriter.addDocument(d, analyzer);
 
+        Integer c = batchCount.get();
+        if ((c + 1) % 64 == 0) {
+            indexWriter.commit();
+        }
 
-	Integer c = batchCount.get();
-	if((c+1) % 100 == 0){
-	    indexWriter.commit();
-	}
-
-	batchCount.set(c+1);
-
+        batchCount.set(c + 1);
 
         return article.getSize();
     }
