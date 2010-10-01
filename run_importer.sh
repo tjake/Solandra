78730e899ff986e4421b00a5c22d01e436d4a96f
Jake Luciani <jake@3.rdrail.net>
2010-10-01 00:08:32 +0000

    fixed logpath

--- run_importer.sh
+++ run_importer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-java -ea -Xmx1G -Xms256M -cp $( echo *.jar lib/*.jar config config| sed 's/ /:/g') lucandra.wikipedia.WikipediaImporter $*
+java -ea -Xmx1G -Xms256M -Djava.util.logging.config.file=config/log4j.properties -cp $( echo config *.jar lib/*.jar | sed 's/ /:/g') lucandra.wikipedia.WikipediaImporter $*
