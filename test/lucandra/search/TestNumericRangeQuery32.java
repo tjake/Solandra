42651a76908b0cafc4f16d2ad5ae7fde571c6cbd
Todd Nine <todd@spidertracks.co.nz>
2010-10-05 09:41:19 +1300

    Removed numeric ranges from ignore

--- test/lucandra/search/TestNumericRangeQuery32.java
+++ test/lucandra/search/TestNumericRangeQuery32.java
@@ -42,7 +42,6 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.util.NumericUtils;
 import org.junit.Ignore;
 
-@Ignore("Ignored until this issue is fixed.  Numeric ranges won't work.  https://issues.apache.org/jira/browse/CASSANDRA-1235")
 public class TestNumericRangeQuery32 extends LucandraTestCase {
 	// distance of entries
 	private static final int distance = 6666;
