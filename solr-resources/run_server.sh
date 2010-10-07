52299cc39f568a5fba7e5f23ab708cf7a23b25fe
Your Name <you@example.com>
2010-10-07 00:05:40 +0000

    get logging properties working

--- solr-resources/run_server.sh
+++ solr-resources/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-REDIS_HOST=localhost
+REDIS_HOST="67.23.43.19"
 MAX_HEAP_SIZE="2G" 
 
 
@@ -28,9 +28,8 @@ JVM_OPTS="$JVM_OPTS -XX:+UseThreadPriorities"
 # http://tech.stolsvik.com/2010/01/linux-java-thread-priorities-workaround.html
 JVM_OPTS="$JVM_OPTS -XX:ThreadPriorityPolicy=42"
 
-
 JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.port=8080" 
 JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.ssl=false" 
 JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.authenticate=false" 
 
-java $JVM_OPTS -Dredis.host=$REDIS_HOST -jar start.jar
+java $JVM_OPTS -Dlog4j.configuration=./log4j.properties -Dredis.host=$REDIS_HOST -jar start.jar
