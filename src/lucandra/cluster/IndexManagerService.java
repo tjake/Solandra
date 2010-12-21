950c830c0ac7a4986d5dd95053c8c76099075f1b
T Jake Luciani <jakers@gmail.com>
2010-12-21 14:48:28 -0500

    shards at once

--- src/lucandra/cluster/IndexManagerService.java
+++ src/lucandra/cluster/IndexManagerService.java
@@ -23,7 +23,7 @@ package lucandra.cluster;
 public class IndexManagerService
 {
     public static final CassandraIndexManager instance;
-    public static final Integer               shardsAtOnce = Integer.valueOf(System.getProperty("shards.at.once", "1"));
+    public static final Integer               shardsAtOnce = Integer.valueOf(System.getProperty("shards.at.once", "4"));
 
     static
     {   
